ef2217c6d641e7b73c2a10b6242cb85192271d6b
Xian <a0928469303@gmail.com>
2026-08-27 10:16:11 +0800

    修正 Attribute 設定 property 錯誤

--- src/StockFlow.Application/Products/CreateProductRequest.cs
+++ src/StockFlow.Application/Products/CreateProductRequest.cs
@@ -3,9 +3,9 @@ using System.ComponentModel.DataAnnotations;
 namespace StockFlow.Application.Products;
 
 public sealed record CreateProductRequest(
-    [property: Required]
-    [property: MinLength(2)]
+    [param: Required]
+    [param: MinLength(2)]
     string Name,
     string Description,
-    [property: Range(0.01, 999999999)]
+    [param: Range(0.01, 999999999)]
     decimal Price);
